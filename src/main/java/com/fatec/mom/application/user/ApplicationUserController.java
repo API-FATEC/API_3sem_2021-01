61eed41fb6ea69ce15f08a1dc87d6d3e14af3828
TobiasLino <tobias.lino@sonda.com>
2021-03-20 01:04:00 -0300

    [user][controller] adição de ações
    - adição de endpoint de insert de usuário
    - adição de endpoint de ativação de usuário
    - adição de verificação do user logado no sistema

--- src/main/java/com/fatec/mom/application/user/ApplicationUserController.java
+++ src/main/java/com/fatec/mom/application/user/ApplicationUserController.java
@@ -1,17 +1,34 @@
 package com.fatec.mom.application.user;
 
+import com.fatec.mom.domain.user.ApplicationUser;
+import com.fatec.mom.domain.user.ApplicationUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class ApplicationUserController {
 
+    @Autowired
+    private ApplicationUserService userService;
+
     @GetMapping("/all")
     @PreAuthorize(value = "isAuthenticated()")
     public String getAllUsers() {
+        ApplicationUser loggedUser = userService.getLoggedUser();
+        System.out.println(loggedUser);
         return "All Users";
+    }
+
+    @PostMapping("/insert")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ApplicationUser insertNewUser(@RequestBody ApplicationUser user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/update/active/{id}")
+    public ApplicationUser activateUser(@RequestBody ApplicationUser applicationUser, @PathVariable Long id) {
+        return userService.activateUser(applicationUser, id);
     }
 }
