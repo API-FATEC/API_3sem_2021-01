11724c570088c39ebc5dc7c5c5e2c86ccb4199cd
TobiasLino <tobias.lino@sonda.com>
2021-03-20 14:19:53 -0300

    [doc][swagger] páginas do swagger liberadas sem autenticação
    - adição da permissão para acessar páginas do swagger
    - adição da documentação de user controller

--- src/main/java/com/fatec/mom/application/user/ApplicationUserController.java
+++ src/main/java/com/fatec/mom/application/user/ApplicationUserController.java
@@ -2,6 +2,8 @@ package com.fatec.mom.application.user;
 
 import com.fatec.mom.domain.user.ApplicationUser;
 import com.fatec.mom.domain.user.ApplicationUserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -23,11 +25,14 @@ public class ApplicationUserController {
 
     @PostMapping("/insert")
     @PreAuthorize("hasAuthority('ADMIN')")
+    @ApiOperation(value = "Insere um novo usuário.", authorizations = { @Authorization(value = "Somente administradores podem inserir usuários.") })
     public ApplicationUser insertNewUser(@RequestBody ApplicationUser user) {
         return userService.save(user);
     }
 
     @PutMapping("/update/active/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ApiOperation(value = "Ativa um usuário já cadastrado.", authorizations = { @Authorization(value = "Somente administradores podem ativar usuários") })
     public ApplicationUser activateUser(@RequestBody ApplicationUser applicationUser, @PathVariable Long id) {
         return userService.activateUser(applicationUser, id);
     }
