059bf54f29d034bae6870794af7b58a856c0fb73
Devanir <61068102+De472@users.noreply.github.com>
2021-04-07 19:17:57 -0300

    [file][reader] retorna número de abas e pega dados da aba selecionada

--- src/main/java/com/fatec/mom/domain/file/Reader.java
+++ src/main/java/com/fatec/mom/domain/file/Reader.java
@@ -18,13 +18,14 @@ public class Reader {
 
 	@Value("${default-upload-path}")
 	private String defaultPath;
+	private int page = 0;
 
 	//metodo criado para ser usado nos outros metodos, reduzindo o código
 	public Iterator<Row> read(String file) throws IOException{
 		File excelfile = new File(String.format("%s/%s", defaultPath, file));
 		FileInputStream fis = new FileInputStream(excelfile);
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		XSSFSheet sheet = workbook.getSheetAt(0);
+		XSSFSheet sheet = workbook.getSheetAt(page);
 		Iterator<Row> rowit = sheet.rowIterator();
 		workbook.close();
 		fis.close();
@@ -33,6 +34,7 @@ public class Reader {
 
 	//retorna uma lista com os dados de um linha do excel. A linha deve ser informada no index
 	//quando uma cell está vazia é adicionado NULL na lista
+	//a aba/página deve ser informada com setPage, por padrão sempre inicializa na página 0
 	public ArrayList<String> getRow(String file, int index) throws IOException {
 		ArrayList<String> lista = new ArrayList<String>();
 		Iterator<Row> r = read(file);
@@ -58,6 +60,7 @@ public class Reader {
 	}
 
 	//retorna o número de linhas existente na tabela do excel
+	//a aba/página deve ser informada com setPage, por padrão sempre inicializa na página 0
 	public int getSize(String file) throws IOException {
 		Iterator<Row> r = read(file);
 		int counter = 0;
@@ -66,5 +69,19 @@ public class Reader {
 			counter += 1;
 		}
 		return counter;
+	}
+	
+	//retorna o número de abas/páginas do arquivo
+	public int getPages(String file) throws IOException{
+		File excelfile = new File(file);
+		FileInputStream fis = new FileInputStream(excelfile);
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		workbook.close();
+		return workbook.getNumberOfSheets();
+	}
+	
+	//modifica a aba/página que é lida
+	public void setPage(int page) {
+		this.page = page;
 	}
 }
