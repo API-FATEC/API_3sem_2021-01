a54593a28465c21c2ba015c36976ceb0893b178e
Tairik Johnny <tairikjohnny@gmail.com>
2021-04-08 23:21:33 -0300

    Arrumando alguns detalhes da tabela

--- frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
+++ frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
@@ -53,22 +53,22 @@
                         </v-form>
                       </v-col>
                     </v-row>
-                    <br>
-                    <br>
+                    <br />
+                    <br />
                     <v-divider></v-divider>
-                    <br>
-                    <br>
+                    <br />
+                    <br />
                     <v-row>
                       <v-col>
                         <v-data-table
-                          :headers="headers"
+                          :headers="[...headers, ...teste]"
                           :items="desserts"
                           sort-by="calories"
                           class="elevation-1"
                         >
                           <template v-slot:top>
                             <v-toolbar flat>
-                              <v-toolbar-title>My CRUD</v-toolbar-title>
+                              <v-toolbar-title>CodeList</v-toolbar-title>
                               <v-divider
                                 class="mx-4"
                                 inset
@@ -77,15 +77,31 @@
                               <v-spacer></v-spacer>
                               <v-dialog v-model="dialog" max-width="500px">
                                 <template v-slot:activator="{ on, attrs }">
-                                  <v-btn
-                                    color="primary"
-                                    dark
-                                    class="mb-2"
-                                    v-bind="attrs"
-                                    v-on="on"
-                                  >
-                                    New Item
-                                  </v-btn>
+                                  <v-row>
+                                    <v-col></v-col>
+                                    <v-col>
+                                      <v-btn
+                                        color="primary"
+                                        dark
+                                        class="mb-2"
+                                        @click="novaColuna"
+                                      >
+                                        Novo Traço
+                                      </v-btn>
+                                    </v-col>
+                                    <v-col>
+                                      <v-btn
+                                        color="primary"
+                                        dark
+                                        class="mb-2"
+                                        v-bind="attrs"
+                                        v-on="on"
+                                      >
+                                        Novo Bloco
+                                      </v-btn>
+                                    </v-col>
+                                  </v-row>
+                                  <v-spacer></v-spacer>
                                 </template>
                                 <v-card>
                                   <v-card-title>
@@ -97,34 +113,18 @@
                                   <v-card-text>
                                     <v-container>
                                       <v-row>
-                                        <v-col cols="12" sm="6" md="4">
+                                        <v-col
+                                          cols="12"
+                                          sm="6"
+                                          md="4"
+                                          v-for="propriedade in Object.keys(
+                                            editedItem
+                                          )"
+                                          :key="propriedade"
+                                        >
                                           <v-text-field
-                                            v-model="editedItem.name"
-                                            label="Dessert name"
-                                          ></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                          <v-text-field
-                                            v-model="editedItem.calories"
-                                            label="Calories"
-                                          ></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                          <v-text-field
-                                            v-model="editedItem.fat"
-                                            label="Fat (g)"
-                                          ></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                          <v-text-field
-                                            v-model="editedItem.carbs"
-                                            label="Carbs (g)"
-                                          ></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                          <v-text-field
-                                            v-model="editedItem.protein"
-                                            label="Protein (g)"
+                                            v-model="editedItem[propriedade]"
+                                            :label="propriedade"
                                           ></v-text-field>
                                         </v-col>
                                       </v-row>
