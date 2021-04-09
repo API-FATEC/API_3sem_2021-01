8e9e7df4e9623934fbe703c0fd1e94c49876323f
Tairik Johnny <tairikjohnny@gmail.com>
2021-04-09 16:43:35 -0300

    Arrumando a tela de EdiçãoCodeList

--- frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
+++ frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
@@ -138,26 +138,25 @@
                                       text
                                       @click="close"
                                     >
-                                      Cancel
+                                      Cancelar
                                     </v-btn>
                                     <v-btn
                                       color="blue darken-1"
                                       text
                                       @click="save"
                                     >
-                                      Save
+                                      Salvar
                                     </v-btn>
                                   </v-card-actions>
                                 </v-card>
                               </v-dialog>
                               <v-dialog
                                 v-model="dialogDelete"
-                                max-width="500px"
+                                max-width="525px"
                               >
                                 <v-card>
                                   <v-card-title class="headline"
-                                    >Are you sure you want to delete this
-                                    item?</v-card-title
+                                    >Você tem certeza que deseja excluir o item?</v-card-title
                                   >
                                   <v-card-actions>
                                     <v-spacer></v-spacer>
@@ -165,13 +164,13 @@
                                       color="blue darken-1"
                                       text
                                       @click="closeDelete"
-                                      >Cancel</v-btn
+                                      >Cancelar</v-btn
                                     >
                                     <v-btn
                                       color="blue darken-1"
                                       text
                                       @click="deleteItemConfirm"
-                                      >OK</v-btn
+                                      >Sim</v-btn
                                     >
                                     <v-spacer></v-spacer>
                                   </v-card-actions>
