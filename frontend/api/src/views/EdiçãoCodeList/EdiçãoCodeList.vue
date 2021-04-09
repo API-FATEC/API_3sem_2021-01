3cb3ffab92a8d059ba36aafcae9f3f7cb2e39fcf
Tairik Johnny <tairikjohnny@gmail.com>
2021-04-08 22:41:00 -0300

    Criando a estrutura do formulario e da tabela

--- frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
+++ frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
@@ -13,7 +13,188 @@
               <v-card-text>
                 <v-row class="mb-6" no-gutters>
                   <v-col>
-                      
+                    <v-row>
+                      <v-col>
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                          <v-text-field
+                            v-model="name"
+                            :counter="nameCounter"
+                            :rules="nameRules"
+                            label="Nome do documento"
+                            required
+                          ></v-text-field>
+
+                          <v-text-field
+                            v-model="partNumber"
+                            :counter="partNumberCounter"
+                            :rules="partNumberRuler"
+                            label="Part Number"
+                            required
+                          ></v-text-field>
+
+                          <v-btn
+                            :disabled="!valid"
+                            color="primary"
+                            class="mr-4"
+                            @click="sendFile"
+                            id="botao-enviar"
+                          >
+                            Enviar
+                          </v-btn>
+
+                          <v-btn
+                            color="error"
+                            class="mr-4"
+                            @click="reset"
+                            id="botao-limpar"
+                          >
+                            Limpar Formulário
+                          </v-btn>
+                        </v-form>
+                      </v-col>
+                    </v-row>
+                    <br>
+                    <br>
+                    <v-divider></v-divider>
+                    <br>
+                    <br>
+                    <v-row>
+                      <v-col>
+                        <v-data-table
+                          :headers="headers"
+                          :items="desserts"
+                          sort-by="calories"
+                          class="elevation-1"
+                        >
+                          <template v-slot:top>
+                            <v-toolbar flat>
+                              <v-toolbar-title>My CRUD</v-toolbar-title>
+                              <v-divider
+                                class="mx-4"
+                                inset
+                                vertical
+                              ></v-divider>
+                              <v-spacer></v-spacer>
+                              <v-dialog v-model="dialog" max-width="500px">
+                                <template v-slot:activator="{ on, attrs }">
+                                  <v-btn
+                                    color="primary"
+                                    dark
+                                    class="mb-2"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                  >
+                                    New Item
+                                  </v-btn>
+                                </template>
+                                <v-card>
+                                  <v-card-title>
+                                    <span class="headline">{{
+                                      formTitle
+                                    }}</span>
+                                  </v-card-title>
+
+                                  <v-card-text>
+                                    <v-container>
+                                      <v-row>
+                                        <v-col cols="12" sm="6" md="4">
+                                          <v-text-field
+                                            v-model="editedItem.name"
+                                            label="Dessert name"
+                                          ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4">
+                                          <v-text-field
+                                            v-model="editedItem.calories"
+                                            label="Calories"
+                                          ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4">
+                                          <v-text-field
+                                            v-model="editedItem.fat"
+                                            label="Fat (g)"
+                                          ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4">
+                                          <v-text-field
+                                            v-model="editedItem.carbs"
+                                            label="Carbs (g)"
+                                          ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4">
+                                          <v-text-field
+                                            v-model="editedItem.protein"
+                                            label="Protein (g)"
+                                          ></v-text-field>
+                                        </v-col>
+                                      </v-row>
+                                    </v-container>
+                                  </v-card-text>
+
+                                  <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn
+                                      color="blue darken-1"
+                                      text
+                                      @click="close"
+                                    >
+                                      Cancel
+                                    </v-btn>
+                                    <v-btn
+                                      color="blue darken-1"
+                                      text
+                                      @click="save"
+                                    >
+                                      Save
+                                    </v-btn>
+                                  </v-card-actions>
+                                </v-card>
+                              </v-dialog>
+                              <v-dialog
+                                v-model="dialogDelete"
+                                max-width="500px"
+                              >
+                                <v-card>
+                                  <v-card-title class="headline"
+                                    >Are you sure you want to delete this
+                                    item?</v-card-title
+                                  >
+                                  <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn
+                                      color="blue darken-1"
+                                      text
+                                      @click="closeDelete"
+                                      >Cancel</v-btn
+                                    >
+                                    <v-btn
+                                      color="blue darken-1"
+                                      text
+                                      @click="deleteItemConfirm"
+                                      >OK</v-btn
+                                    >
+                                    <v-spacer></v-spacer>
+                                  </v-card-actions>
+                                </v-card>
+                              </v-dialog>
+                            </v-toolbar>
+                          </template>
+                          <template v-slot:item.actions="{ item }">
+                            <v-icon small class="mr-2" @click="editItem(item)">
+                              mdi-pencil
+                            </v-icon>
+                            <v-icon small @click="deleteItem(item)">
+                              mdi-delete
+                            </v-icon>
+                          </template>
+                          <template v-slot:no-data>
+                            <v-btn color="primary" @click="initialize">
+                              Reset
+                            </v-btn>
+                          </template>
+                        </v-data-table>
+                      </v-col>
+                    </v-row>
                   </v-col>
                 </v-row>
               </v-card-text>
@@ -25,11 +206,10 @@
   </div>
 </template>
 
-<script>
-</script>
+<script src="./EdiçãoCodeList.js" ></script>
 
 <style>
-#titulo{
-    margin-top: 10px;
+#titulo {
+  margin-top: 10px;
 }
 </style>
