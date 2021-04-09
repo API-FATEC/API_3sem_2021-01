7afe39852823e0b8079d1d697aa050fa6b53c3cc
Tairik Johnny <tairikjohnny@gmail.com>
2021-04-08 22:00:03 -0300

    Adicionando a estrutura padrão ao arquivo

--- frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
+++ frontend/api/src/views/EdiçãoCodeList/EdiçãoCodeList.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>Testando a tela</h1>
+  <div>
+    <v-col>
+      <v-row justify="center" class="pa-2">
+        <div id="titulo">
+          <titulo class="display-1">Edição CodeList</titulo>
+        </div>
+      </v-row>
+      <v-row class="pa-4">
+        <v-col>
+          <v-card class="pa-6" color="#0266B1">
+            <v-card class="pa-2" tile outlined color="white">
+              <v-card-text>
+                <v-row class="mb-6" no-gutters>
+                  <v-col>
+                      
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </div>
 </template>
+
+<script>
+</script>
+
+<style>
+#titulo{
+    margin-top: 10px;
+}
+</style>
