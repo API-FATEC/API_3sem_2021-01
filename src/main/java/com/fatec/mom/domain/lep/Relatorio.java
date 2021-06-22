df900fc8030348a0c8b6897eb7dac9da4cbcae97
Tairik Johnny <tairikjohnny@gmail.com>
2021-06-21 23:45:32 -0300

    Criando a interface do relatorio

---
+++ src/main/java/com/fatec/mom/domain/lep/Relatorio.java
@@ -0,0 +1,8 @@
+package com.fatec.mom.domain.lep;
+
+public interface Relatorio {
+    public void gerarCabecalho();
+    //public void gerarCorpo();
+    //public void gerarRodape();
+    public void imprimir();
+}
