43f0199efaf86fd8394416fbe961b57597b7387c
Tairik Johnny <tairikjohnny@gmail.com>
2021-06-22 22:52:54 -0300

    Criando a classe com os atributos da LEP

--- src/main/java/com/fatec/mom/domain/lep/ItensLEP.java
+++ src/main/java/com/fatec/mom/domain/lep/ItensLEP.java
@@ -1,2 +1,18 @@
-package com.fatec.mom.domain.lep;public class ItensLEP {
+package com.fatec.mom.domain.lep;
+
+import lombok.Data;
+
+@Data
+public class ItensLEP {
+    private String block;
+    private int code;
+    private int page;
+    private String change;
+
+    public ItensLEP(String block, int code, int page, String change) {
+        this.block = block;
+        this.code = code;
+        this.page = page;
+        this.change = change;
+    }
 }
