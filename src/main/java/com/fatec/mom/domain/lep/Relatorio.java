1fe28b0ddb876339cda4f35ab62c7ce1ce175fc3
Tairik Johnny <tairikjohnny@gmail.com>
2021-06-22 22:54:26 -0300

    Criando novos métodos e alterando o nome dos métodos que já existiam

--- src/main/java/com/fatec/mom/domain/lep/Relatorio.java
+++ src/main/java/com/fatec/mom/domain/lep/Relatorio.java
@@ -1,8 +1,10 @@
 package com.fatec.mom.domain.lep;
 
 public interface Relatorio {
-    public void gerarCabecalho();
-    //public void gerarCorpo();
+    public void gerarCabecalhoPaginaDeRevisao();
+    public void gerarCorpoPaginaDeRevisao();
+    public void gerarCabecalhoPaginaDosBlocos();
+    public void gerarCorpoPaginaDosBlocos();
     //public void gerarRodape();
     public void imprimir();
 }
